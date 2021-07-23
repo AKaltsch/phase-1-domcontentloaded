0936cb8effe349954a6432ece3696dff5595d1c6
alec <kaltschmidalec@gmail.com>
2021-07-23 14:30:45 -0700

    commit tree 8fb2644ae480589f18779bce4891a6739bae867c

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+
+// document.addEventListener('DOMContentLoaded', () =>{
+//     const p = document.querySelector('#text')
+//     console.log(p.textContent = 'This is really cool!')
+
+// })
+
+// console.log('before other text')
+document.addEventListener('DOMContentLoaded', () => {
+    let p = document.querySelector('p')
+    console.log(p.textContent = 'This is really cool!')
+})
